d03215aff7a5161569b55dade4d85954c5d616ab
Natalia Kvaskova <dreamstrueok@gmail.com>
2023-11-09 16:46:35 +0300

    Добавлен блок-обертка для контейнера с фото. Добавлены общие стили.

--- js/app.js
+++ js/app.js
@@ -7,11 +7,15 @@ for(let i = 0; i < countPhotoSet; i++){
 let main = document.querySelector('main');
 
 for (i = 0; i < countSet; i++){
+  let wrapper = document.createElement ('div');
+  wrapper.classList.add("wrapper");
+  main.append(wrapper);
   let div = document.createElement ('div');
-  main.append(div);
-    setPhoto.forEach ((photo, index) =>{
+  div.classList.add("setPhoto", `set-${i+1}`);
+  wrapper.append(div);
+    setPhoto.forEach ((photo) =>{
       let strPhotoCard = `
-        <div class = "setPhoto__Photo" style="background-image: url('img/${setPhoto[index]}');"></div>
+        <div class = "setPhoto__Photo" style="background-image: url('img/${photo}');"></div>
         `
         div.insertAdjacentHTML("beforeend", strPhotoCard);
     })
